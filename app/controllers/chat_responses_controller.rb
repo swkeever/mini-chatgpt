8000f912e53d39aebddc5ee61f9f4ebbad8f80a5
Sean Keever <swkeever@gmail.com>
2023-09-27 14:29:38 -0400

    Update

--- app/controllers/chat_responses_controller.rb
+++ app/controllers/chat_responses_controller.rb
@@ -2,20 +2,29 @@ class ChatResponsesController < ApplicationController
   include ActionController::Live
 
   def show
-    response.headers['Content-Type'] = 'text/event-stream'
-    sse                              = SSE.new(response.stream, retry: 300, event: "chat_response")
-    client                           = OpenAI::Client.new(access_token: ENV["OPENAI_ACCESS_TOKEN"])
+    response.headers['Content-Type']  = 'text/event-stream'
+    response.headers['Last-Modified'] = Time.now.httpdate
+    sse                               = SSE.new(response.stream, event: "message")
+    client                            = OpenAI::Client.new(access_token: ENV["OPENAI_ACCESS_TOKEN"])
     begin
       client.chat(
         parameters: {
           model:    "gpt-3.5-turbo",
-          messages: [{ role: "user", content: params[:prompt] }],
+          messages: [
+                      { role: "user", content: params[:prompt] }],
           stream:   proc do |chunk|
             content = chunk.dig("choices", 0, "delta", "content")
-            sse.write(content)
+            if content.nil?
+              return
+            end
+            print content
+            sse.write({
+                        message: content,
+                      })
           end
         }
       )
+
     ensure
       sse.close
     end
